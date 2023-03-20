0a2fb46736a53623b3b3cae08c1cbff1883368e2
atevsl <108408902+atevsl@users.noreply.github.com>
2023-03-20 16:13:34 +0000

    Deploying to gh-pages from @ atevsl/goit-react-hw-08-phonebook@e7427b96cb872e336d02040743621a7e55a2f5fd 🚀

---
+++ static/js/415.09eae779.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,r){r.r(t);var a=r(3329);t.default=function(){return(0,a.jsx)("div",{children:"Home Page"})}}}]);
+//# sourceMappingURL=415.09eae779.chunk.js.map
